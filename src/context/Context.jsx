711c18e5838da3c26558aa8f717f6aa853fefe57
[PeradheepY] <[peradheep200617@outlook.com]>
2024-08-02 17:58:19 +0530

    gemini-prjt

---
+++ src/context/Context.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompst, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+
+
+
+    const onSent = async (prompt) => {
+        await runChat(prompt)
+    }
+    
+    onSent("What is react js")
+    
+    const contextValue = {
+        
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider;
