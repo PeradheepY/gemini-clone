e15f2996af7bf56a7415a7e88f406d395d494f71
[PeradheepY] <[peradheep200617@outlook.com]>
2024-08-03 15:47:05 +0530

    updated

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,41 +1,32 @@
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- *
- * See the getting started guide for more information
- * https://ai.google.dev/gemini-api/docs/get-started/node
- */
-// require('dotenv').config();
 
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-  
-  // eslint-disable-next-line no-undef
-  const apiKey = "AIzaSyDvty2yzsztIECxubWI9puYnbLYREMNG94";
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.0-pro",
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Access the API key from the environment variable
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.0-pro",
+});
+
+const generationConfig = {
+  temperature: 0.9,
+  topP: 1,
+  maxOutputTokens: 2048,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
   });
-  
-  const generationConfig = {
-    temperature: 0.9,
-    topP: 1,
-    maxOutputTokens: 2048,
-    responseMimeType: "text/plain",
-  };
-  
-  async function runChat(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-  }
-  
-  export default runChat;
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default runChat;
